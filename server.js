ac34aa00773a93578b6aee525c95a6c9a69ffaa1
Chris Guenther <cmguenther32@gmail.com>
2022-07-15 11:58:31 -0400

    database fix

--- server.js
+++ server.js
@@ -12,9 +12,7 @@ const db = knex({
   client: "pg",
   connection: {
     connectionString: process.env.DATABASE_URL,
-    user: "postgres",
-    password: "Cabbage15",
-    ssl: true,
+    ssl: { rejectUnauthorized: false },
   },
 });
 
