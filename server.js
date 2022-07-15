0ec7e567b40b50a35f3885a6a5b24250fc70c946
Chris Guenther <cmguenther32@gmail.com>
2022-07-15 10:44:12 -0400

    updating

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const db = knex({
   client: "pg",
   connection: {
     host: "127.0.0.1",
-    // port: 3306,
+    port: 5432,
     user: "postgres",
     password: "Cabbage15",
     database: "smart-brain",
